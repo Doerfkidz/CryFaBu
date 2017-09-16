4f43830e26531e76de84457e2c188b3d1e42b706
Tunk <Tunk@DESKTOP-F9F1G62>
2017-09-16 22:01:49 +0200

    Liste

--- CryFaBu/CryFaBu/frm_main.vb
+++ CryFaBu/CryFaBu/frm_main.vb
@@ -6,4 +6,8 @@
     Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
 
     End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles tbox_gewinn.TextChanged
+
+    End Sub
 End Class
