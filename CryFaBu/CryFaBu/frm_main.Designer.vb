f5168aa7267aaee7e08d8458f5f571377aba0b6d
Tunk <Tunk@DESKTOP-F9F1G62>
2017-09-16 21:35:49 +0200

    test

--- CryFaBu/CryFaBu/frm_main.Designer.vb
+++ CryFaBu/CryFaBu/frm_main.Designer.vb
@@ -22,18 +22,55 @@ Partial Class frm_main
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.btn_start = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
         Me.SuspendLayout()
+        '
+        'btn_start
+        '
+        Me.btn_start.Location = New System.Drawing.Point(280, 253)
+        Me.btn_start.Name = "btn_start"
+        Me.btn_start.Size = New System.Drawing.Size(317, 154)
+        Me.btn_start.TabIndex = 0
+        Me.btn_start.Text = "Start"
+        Me.btn_start.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(136, 390)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(62, 17)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "Sicher?"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckedListBox1
+        '
+        Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Location = New System.Drawing.Point(673, 156)
+        Me.CheckedListBox1.Name = "CheckedListBox1"
+        Me.CheckedListBox1.Size = New System.Drawing.Size(8, 4)
+        Me.CheckedListBox1.TabIndex = 2
         '
         'frm_main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(939, 419)
+        Me.Controls.Add(Me.CheckedListBox1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.btn_start)
         Me.Name = "frm_main"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "^^"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents btn_start As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckedListBox1 As CheckedListBox
 End Class
--- CryFaBu/CryFaBu/frm_main.vb
+++ CryFaBu/CryFaBu/frm_main.vb
@@ -1,3 +1,9 @@
 ﻿Public Class frm_main
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btn_start.Click
 
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+
+    End Sub
 End Class
